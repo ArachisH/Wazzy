ee8e5552761d318834f1d846a1eabd94346e2367
ArachisH <iKBenPeanut@gmail.com>
2020-11-02 18:47:32 -0800

    * Have all complex WASM data types inherit from this class

---
+++ Wazzy/IO/WASMObject.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace Wazzy.IO
+{
+    public abstract class WASMObject
+    {
+        public abstract void WriteTo(WASMWriter output);
+
+        public static byte[] ToBytes<T>(IList<T> wasmObjs) where T : WASMObject
+        {
+            if (wasmObjs.Count == 0) return new byte[0];
+
+            using var output = new MemoryStream();
+            using var wasmOutput = new WASMWriter(output);
+            foreach (WASMObject wasmObj in wasmObjs)
+            {
+                wasmObj.WriteTo(wasmOutput);
+            }
+            wasmOutput.Flush();
+            return output.ToArray();
+        }
+        public static byte[] ToBytes(WASMObject wasmObj) => ToBytes(new[] { wasmObj }); // Whatever, don't @ me
+    }
+}
