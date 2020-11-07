27a4270edde924bbb5ddd9c76271721cf7776b2c
ArachisH <iKBenPeanut@gmail.com>
2020-11-07 15:42:19 -0800

    * This is an int I think, not a byte

--- Wazzy/Types/Local.cs
+++ Wazzy/Types/Local.cs
@@ -4,21 +4,27 @@ using Wazzy.IO;
 
 namespace Wazzy.Types
 {
-    public class Local :  WASMType
+    public class Local : WASMType
     {
-        public int N { get; }
+        private readonly int _typeIndex = -1;
+
+        public int Rank { get; }
         public Type Type { get; }
 
         public Local(WASMModule module)
         {
-            N = module.Input.Read7BitEncodedInt();
-            Type = module.Input.ReadValueType();
+            Rank = module.Input.Read7BitEncodedInt();
+            _typeIndex = module.Input.Read7BitEncodedInt();
+            if (IsSupportedType(_typeIndex))
+            {
+                Type = GetType(_typeIndex);
+            }
         }
 
         public override void WriteTo(WASMWriter output)
         {
-            output.Write7BitEncodedInt(N);
-            output.Write(Type);
+            output.Write7BitEncodedInt(Rank);
+            output.Write7BitEncodedInt(_typeIndex);
         }
     }
 }
