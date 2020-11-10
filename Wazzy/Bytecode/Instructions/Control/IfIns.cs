00c05fe64606290154a5fe6570054a99f32db7ac
ArachisH <iKBenPeanut@gmail.com>
2020-11-09 19:59:48 -0800

    * Handle the expression of an Else instruction if it appears

--- Wazzy/Bytecode/Instructions/Control/IfIns.cs
+++ Wazzy/Bytecode/Instructions/Control/IfIns.cs
@@ -10,12 +10,17 @@ namespace Wazzy.Bytecode.Instructions.Control
     {
         public int BlockId { get; set; }
         public Type BlockType { get; private set; }
+
         public List<WASMInstruction> Expression { get; }
+        public List<WASMInstruction> ElseExpression { get; }
+
+        public bool HasElseExpression => ElseExpression?.Count > 0;
 
         public IfIns()
             : base(OPCode.If)
         {
             Expression = new List<WASMInstruction>();
+            ElseExpression = new List<WASMInstruction>();
         }
         public IfIns(WASMReader input)
             : base(OPCode.If)
@@ -26,15 +31,26 @@ namespace Wazzy.Bytecode.Instructions.Control
                 BlockType = WASMType.GetType((byte)BlockId);
             }
             else if (BlockId == 0x40) BlockType = typeof(void);
-            else System.Diagnostics.Debugger.Break();
 
-            Expression = input.ReadExpression();
+            Expression = input.ReadExpression(OPCode.Else);
+            if (Expression[^1].OP == OPCode.Else)
+            {
+                ElseExpression = input.ReadExpression();
+                Expression.RemoveAt(Expression.Count - 1);
+            }
         }
 
         protected override void WriteBodyTo(WASMWriter output)
         {
             output.Write7BitEncodedInt(BlockId);
             foreach (WASMInstruction instruction in Expression)
+            {
+                instruction.WriteTo(output);
+            }
+            if (ElseExpression.Count == 0) return;
+
+            output.Write((byte)OPCode.Else);
+            foreach (WASMInstruction instruction in ElseExpression)
             {
                 instruction.WriteTo(output);
             }
